ab37d464d9149d1234ff1c8e2193c98881dd1523
kalleva <kaluzhinlev@gmail.com>
2017-05-06 17:42:46 +0300

    parts filled with color translated from area to color animation

--- triangle/js/myScript.js
+++ triangle/js/myScript.js
@@ -1,10 +1,10 @@
-// triangle:
-//          B
-//        /  \
-//       /    \
-//      K      L
-//     /  * O * \
-//    /  *    *  \
+// triangle ABC, point O is intersection of CK and AL:
+//  hB     B
+//  |     /  \
+//  hL   /    L
+//  hK  K    * \
+//  hO /  * O   \
+//  | /  *    *  \
 //   A ---------- C
 
 var rectangle = {};
@@ -25,40 +25,105 @@ function next_frame(timestamp, distance, direction) {
 
 function draw(distance, direction) {
   // debugger;
-  var canvas_width = 500;
-  var canvas_height = 500;
+  var canvasWidth = 500;
+  var canvasHeight = 500;
   var ctx = document.querySelector('canvas').getContext('2d');
-  ctx.clearRect(0, 0, canvas_width, canvas_height); // clear canvas before drawing new frame
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight); // clear canvas before drawing new frame
   ctx.save();
+
+  ctx.transform(1, 0, 0, -1, 0, canvasHeight); // flip coordinate axes
   ctx.translate(50, 50); // move origin of coordinat
 
+  var pA = point(0, 0);
+  var pB = point(120, 400);
+  var pC = point(400, 0);
+  var fullDist = 400;
+
+
+  var fullArea = 1 / 2 * (pB.x + pC.x) * pB.y - 1 / 2 * (pB.x * pB.y);
+
+  pK = linearMotion(pB, pA, distance / fullDist);
+  pL = linearMotion(pB, pC, distance / fullDist);
+
+  var pO = intersection(pA, pL, pC, pK);
+  // console.log(pO);
+  var phO = point(0, pO.y);
+  var phK = point(0, pK.y);
+  var phL = point(0, pL.y);
+  var phB = point(0, pB.y);
+
+  // Computes areas of sectors AOC, AOK, LOC, BLOC
+  // To compute areas can use projections of points on vertical line going through
+  // point A
+  // We can represent areas we wish to find as:
+  // s_AOC = s_AhOOC - s_AOhO
+  // s_AOK = s_AOhO + s_hKKOhO - s_AKhK
+  // s_LOC = s_ACLhL - s_OhOLhL - s_AhOOC
+  // s_BLOC = s_OhOLhL + s_hBBLhL - s_hKKOhO - s_BhBhKK
+
+  var s_AOhO = 1 / 2 * (phO.y * pO.x);
+  var s_AhOOC = 1 / 2 * (pO.x + pC.x) * phO.y;
+  var s_hKKOhO = 1 / 2 * (pK.x + pO.x) * (phK.y - phO.y);
+  var s_AKhK = 1 / 2 * (phK.y * pK.x);
+  var s_ACLhL = 1 / 2 * (pL.x + pC.x) * phL.y;
+  var s_OhOLhL = 1 / 2 * (pL.x + pO.x) * (phL.y - phO.y);
+  var s_hBBLhL = 1 / 2 * (pB.x + pL.x) * (phB.y - phL.y);
+  var s_BhBhKK = 1 / 2 * (pB.x + pK.x) * (phB.y - phK.y);
+
+  var s_AOC = s_AhOOC - s_AOhO;
+  var s_AOK = s_AOhO + s_hKKOhO - s_AKhK;
+  var s_LOC = s_ACLhL - s_OhOLhL - s_AhOOC;
+  var s_BLOC = s_OhOLhL + s_hBBLhL - s_hKKOhO - s_BhBhKK;
+
+  // draws AOC, AOK, LOC, BLOK and fills them with color wich is computed from
+  // ther area
   ctx.beginPath();
-  ctx.moveTo(0, 400);
-  ctx.lineTo(120, 0);
-  ctx.lineTo(400, 400);
-  ctx.lineTo(0, 400);
+  ctx.moveTo(pA.x, pA.y);
+  ctx.lineTo(pO.x, pO.y);
+  ctx.lineTo(pC.x, pC.y);
+  ctx.fillStyle = areaToColor(s_AOC, fullArea);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(pA.x, pA.y);
+  ctx.lineTo(pO.x, pO.y);
+  ctx.lineTo(pK.x, pK.y);
+  ctx.fillStyle = areaToColor(s_AOK, fullArea);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(pC.x, pC.y);
+  ctx.lineTo(pO.x, pO.y);
+  ctx.lineTo(pL.x, pL.y);
+  ctx.fillStyle = areaToColor(s_LOC, fullArea);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(pB.x, pB.y);
+  ctx.lineTo(pL.x, pL.y);
+  ctx.lineTo(pO.x, pO.y);
+  ctx.lineTo(pK.x, pK.y);
+  ctx.fillStyle = areaToColor(s_BLOC, fullArea);
+  ctx.fill();
+
+  //draws AL and CK
+  ctx.moveTo(pK.x, pK.y);
+  ctx.lineTo(pC.x, pC.y);
+  ctx.moveTo(pL.x, pL.y);
+  ctx.lineTo(pA.x, pA.y);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(pA.x, pA.y);
+  ctx.lineTo(pB.x, pB.y);
+  ctx.lineTo(pC.x, pC.y);
+  ctx.lineTo(pA.x, pA.y);
   ctx.strokeStyle = 'black';
   ctx.stroke();
 
-
-  k_coord = linearMotion(120, 0, 0, 400, distance / 400);
-  l_coord = linearMotion(120, 0, 400, 400, distance / 400);
-
-  ctx.moveTo(k_coord.x, k_coord.y);
-  ctx.lineTo(400, 400);
-  ctx.moveTo(l_coord.x, l_coord.y);
-  ctx.lineTo(0, 400);
-  ctx.stroke();
-
-  ctx.fillStyle = 'green';
-  ctx.fillRect(k_coord.x - 3, k_coord.y - 2, 5, 5);
-
-  ctx.fillStyle = 'red';
-  ctx.fillRect(l_coord.x - 1, l_coord.y - 2, 5, 5);
-
   // decide position for the next frame if it equals length then reverse direction
   if (direction === 1) {
-    if (distance < 400) {
+    if (distance < fullDist) {
       distance += 1;
     } else {
       direction = -1;
@@ -89,18 +154,38 @@ function getRandomInt(min, max) {
 // input: beginning and end of line and percent of passed distance from lines
 // total length
 // output: coordinates of the point
-function linearMotion(x0, y0, x1, y1, distance) {
-dx = x1 - x0;
-dy = y1 - y0;
-xi = x0 + dx * distance;
-yi = y0 + dy * distance;
-return { x: xi, y: yi };
+function linearMotion(beginning, end, distance) {
+dx = end.x - beginning.x;
+dy = end.y - beginning.y;
+xi = beginning.x + dx * distance;
+yi = beginning.y + dy * distance;
+return point(xi, yi);
 }
 
 // computes color from area, smaller the area resuls darker color
-function areaToColor(x, y, full_area) {
-  var color = Math.floor(20 + (x * y / full_area) * 170);
+function areaToColor(sector, fullArea) {
+  var color = Math.floor(20 + (sector / fullArea) * 170);
   return "rgb(" + color + ", " + color + ", " + 255 + ")";
+}
+
+function point(xi, yi) {
+  return {x: xi, y: yi};
+}
+
+// find intersection of two lines
+// fist line - points p11, p12
+// second line - points p21, p22
+function intersection(p11, p12, p21, p22){
+  // debugger;
+  var i = point(0, 0);
+  var slope1 = (p12.y - p11.y) / (p12.x - p11.x);
+  var slope2 = (p22.y - p21.y) / (p22.x - p21.x);
+  i.x = (slope1 * p11.x - p11.y - slope2 * p21.x + p21.y) / (slope1 - slope2);
+  if (isNaN(i.x)) {
+    i.x = (p12.x + p22.x) / 2;
+  }
+  i.y = slope1 * (i.x - p11.x) + p11.y;
+  return i;
 }
 
 rectangle.init();
